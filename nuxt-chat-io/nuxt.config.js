aa40f36538b195824610e7ffb681ece9b81fa8fa
shutemov <shutemov1997@mail.ru>
2020-01-19 11:26:12 +0300

    Добавлен node-sass и sass-loader. Требуется npm install.

--- nuxt-chat-io/nuxt.config.js
+++ nuxt-chat-io/nuxt.config.js
@@ -23,7 +23,8 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    '@/theme/index.scss'
   ],
   /*
   ** Plugins to load before mounting the App
