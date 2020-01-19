d512ba332bc31d7813bf05a2bad2ccbef30b9be6
shutemov <shutemov1997@mail.ru>
2020-01-19 18:00:17 +0300

    Добавлен layout авторизации.

---
+++ nuxt-chat-io/layouts/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-container>
+    <el-header>Chat.IO</el-header>
+    <el-main style="margin:0">
+      <nuxt/>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+
+  export default {
+    name: "login",
+  }
+</script>
+
+<style scoped>
+
+
+</style>
--- nuxt-chat-io/pages/index.vue
+++ nuxt-chat-io/pages/index.vue
@@ -1,49 +1,21 @@
 <template>
   <el-row type="flex" justify="center">
-    <el-col :xs="24" :sm="18" :md="12" :lg="10">
-      <room-list></room-list>
+    <el-col :xs='18' :sm='12' :md="8" :lg="8">
+      <auth-app></auth-app>
     </el-col>
   </el-row>
 </template>
 
 <script>
-
-  import RoomList from './../components/main/mainPage/RoomList'
+  import AuthApp from './../components/main/login/authApp'
 
   export default {
-    components: {RoomList}
+    name: "index",
+    layout: "login",
+    components: {AuthApp}
   }
 </script>
 
-<style>
-  .container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
+<style scoped>
 
-  .title {
-    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
-  }
 </style>
