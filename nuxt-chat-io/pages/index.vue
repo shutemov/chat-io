096127c78f84294026ee18d5783331fa16fbe784
shutemov <shutemov1997@mail.ru>
2020-01-19 09:05:24 +0300

    Настройка проекта
    Добавлен global.js файл для подключения плагинов.

--- nuxt-chat-io/pages/index.vue
+++ nuxt-chat-io/pages/index.vue
@@ -29,11 +29,11 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+
 
 export default {
   components: {
-    Logo
+
   }
 }
 </script>
---
+++ nuxt-chat-io/plugins/globals.js
@@ -0,0 +1,10 @@
+/*
+Подключючаем все плагины, использующиеся в приложении.
+ */
+
+
+import Vue from 'vue'
+import Element from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ru-RU'
+
+Vue.use(Element, { locale })
