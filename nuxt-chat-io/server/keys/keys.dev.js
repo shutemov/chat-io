035a1e214c304220a5273e305340b554923f0128
shutemov <shutemov1997@mail.ru>
2020-02-02 16:38:23 +0300

    Update keys.dev.js

--- nuxt-chat-io/server/keys/keys.dev.js
+++ nuxt-chat-io/server/keys/keys.dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGO_URI: 'mongodb+srv://root:1234@chat-io-claster-gmhkp.mongodb.net/test?retryWrites=true&w=majority',
+  MONGO_URI: 'mongodb+srv://root:1234@chat-io-claster-gmhkp.mongodb.net/chat-io-DB?retryWrites=true&w=majority',
   JWT: 'JWToken'
 }
